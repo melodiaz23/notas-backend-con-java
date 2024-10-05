edeae586d9673ca91ce34d53d70fa4e5d02908ff
meli.diaz23 <meli.diaz23@gmail.com>
2024-10-04 22:19:09 -0500

    Actividades primeros pasos con java

---
+++ actividades-java/1_primeros-pasos/Ejercicio2.java
@@ -0,0 +1,12 @@
+public class Ejercicio2 {
+  public static void main(String[] args) {
+    String mensajeBienvenida = "Bienvenido a Java";
+    double temperaturaActual = 19;
+    boolean datoLogico = true;
+
+    System.out.println(
+        mensajeBienvenida + ". La temperatura actual es " + temperaturaActual + " y el dato lógico ingresado es "
+            + datoLogico);
+  }
+
+}
---
+++ actividades-java/1_primeros-pasos/MiPrimerPrograma.java
@@ -0,0 +1,17 @@
+public class MiPrimerPrograma {
+  // psvm
+  public static void main(String[] args) {
+    String nombre = "Melissa";
+    int edad = 32;
+    // boolean esVerdadero = false;
+    char letra = 'm';
+    long number = 23988888888L;
+    float number2 = 23.5f;
+    System.out.println("Bienvenido a tu curso de Java");
+    System.out.println("Mi nombre es: " + nombre + " y tengo " + edad + " años");
+    System.out.println("Estas son 3 variables:");
+    System.out.println(letra);
+    System.out.println(number);
+    System.out.println(number2);
+  }
+}
