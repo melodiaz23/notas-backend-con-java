664f1e48521578139aaeeba62f0ff67cb7c0a2ee
Melissa Diaz <meli.diaz23@gmail.com>
2024-11-11 16:57:44 -0500

    Actuvidad: Métodos II - Random Number

---
+++ actividades-java/20_metodos-ii/Random.java
@@ -0,0 +1,13 @@
+// Generación de números aleatorios: Basándote en el ejercicio que ya has realizado, crea un método "numeroAleatorio()" que no requiera parámetros y devuelva un "int" dentro del rango especificado.
+// Ejercicio anterior: “Generando números aleatorios” Escribe un programa que genere y muestre en pantalla un número aleatorio en el rango del 1 al 355  utilizando el método random() de la clase Math. Puedes utilizar el método floor() para redondear el número aleatorio hacia abajo.
+public class Random {
+  public static void main(String[] args) {
+    System.out.println(generarAleatorio());
+  }
+
+  public static int generarAleatorio() {
+    int numRamdom = (int) (1 + (Math.random() * 355));
+    return numRamdom;
+  }
+
+}
