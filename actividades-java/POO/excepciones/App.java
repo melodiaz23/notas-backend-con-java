e6d67b350e177d058ad88e85e03bb56e7677c36a
Melissa Diaz <meli.diaz23@gmail.com>
2024-11-28 18:12:07 -0500

    Actividad: Saludo

---
+++ actividades-java/POO/excepciones/App.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class App {
+  public static Scanner scanner = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    try {
+      solicitarNombre(scanner);
+    } catch (MiExcepcion e) {
+      System.out.println("Error: " + e.getMessage());
+    } catch (Exception e) {
+      System.out.println("Error inesperado: " + e);
+    } finally {
+      scanner.close();
+    }
+  }
+
+  public static void solicitarNombre(Scanner scanner) throws MiExcepcion {
+    System.out.println("Por favor, indica tu nombre: ");
+    String nombre = scanner.nextLine();
+    if (nombre.length() < 2) {
+      throw new MiExcepcion("El nombre ingresado debe ser mayor a 2 caracteres");
+    }
+    System.out.println("Hola, " + nombre);
+  }
+}
---
+++ actividades-java/POO/excepciones/MiExcepcion.java
@@ -0,0 +1,6 @@
+public class MiExcepcion extends Exception {
+
+  public MiExcepcion(String mensaje) {
+    super(mensaje);
+  }
+}
