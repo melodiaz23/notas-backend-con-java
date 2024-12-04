45f5a88be3d031e143348f22ddad7d42a44dfa6f
Melissa Diaz <120139230+melodiaz23@users.noreply.github.com>
2024-12-04 18:44:28 -0500

    Update RegistroDeInvitados.java

--- actividades-java/java-collections-framework/listadeinvitados/RegistroDeInvitados.java
+++ actividades-java/java-collections-framework/listadeinvitados/RegistroDeInvitados.java
@@ -1,1 +1,25 @@
-package listadeinvitados;import java.util.HashSet;public class RegistroDeInvitados {	private final HashSet<String> nombreInvitados = new HashSet<>();	public void agregarInvitado(String nombre){		System.out.println(nombreInvitados.add(nombre.toUpperCase()) ?						"Nombre fue adicionado" :						"Nombre ya existe");	}	public void eliminarInvitado(String nombre){		System.out.println(nombreInvitados.remove(nombre.toUpperCase()) ?						"Nombre fue eliminado":						"Nombre no existe");	}	public void mostrarInvitados(){		System.out.println(nombreInvitados);	}}
+package listadeinvitados;
+
+import java.util.HashSet;
+
+public class RegistroDeInvitados {
+	private final HashSet<String> nombreInvitados = new HashSet<>();
+
+	public void agregarInvitado(String nombre){
+		System.out.println(nombreInvitados.add(nombre.toUpperCase()) ?
+						"Nombre fue adicionado" :
+						"Nombre ya existe");
+	}
+
+	public void eliminarInvitado(String nombre){
+		System.out.println(nombreInvitados.remove(nombre.toUpperCase()) ?
+						"Nombre fue eliminado":
+						"Nombre no existe");
+	}
+
+	public void mostrarInvitados(){
+		System.out.println(nombreInvitados);
+	}
+
+
+}
