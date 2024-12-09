0619e3af7e0d01d0131af4c9171901abf2a79466
Melissa Diaz <meli.diaz23@gmail.com>
2024-12-08 19:16:05 -0500

    Ajuste en actividad: Suma de números (mejora en visualización)

--- actividades-java/java-collections-framework/sopadenumeros/Run.java
+++ actividades-java/java-collections-framework/sopadenumeros/Run.java
@@ -1,1 +1,52 @@
-package sopadenumeros;import java.util.ArrayList;import java.util.Iterator;import java.util.Scanner;public class Run{	public static void main(String[] args) {		ArrayList<Integer> numeros = leerValores();		mostrarResultados(numeros, calcularSuma(numeros), calcularPromedio(numeros));	}	public static ArrayList<Integer> leerValores(){		Scanner sc= new Scanner(System.in);		ArrayList<Integer> valores = new ArrayList<>();		int entrada=0;		while (entrada!=-99) {			System.out.println("Ingrese un número (-99 para salir)");			entrada = sc.nextInt();			if (entrada!=-99) valores.add(entrada);		}		return valores;	}	public static int calcularSuma(ArrayList<Integer> numeros){		int suma = 0;		Iterator<Integer> iteratorNumeros = numeros.iterator();		while (iteratorNumeros.hasNext()) {			suma += iteratorNumeros.next();		}		return suma;	}	public static int calcularPromedio(ArrayList<Integer> numeros){		int promedio;		int sumaDeNumeros = calcularSuma(numeros);		promedio = sumaDeNumeros/numeros.size();		return promedio;	}	public static void mostrarResultados(ArrayList<Integer> numeros,int suma, int media){		int numerosMayoresMedia = 0;    for (Integer numero : numeros) {      if (numero > media) {        numerosMayoresMedia++;      }    }		System.out.printf("Datos: %s \nSuma: %d \nMedia: %d \nCantidad mayores a media: %d", numeros, suma, media, numerosMayoresMedia);	}}
+package sopadenumeros;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Run{
+	public static void main(String[] args) {
+		ArrayList<Integer> numeros = leerValores();
+
+		mostrarResultados(numeros, calcularSuma(numeros), calcularPromedio(numeros));
+
+	}
+	public static ArrayList<Integer> leerValores(){
+		Scanner sc= new Scanner(System.in);
+		ArrayList<Integer> valores = new ArrayList<>();
+		int entrada=0;
+		while (entrada!=-99) {
+			System.out.println("Ingrese un número (-99 para salir)");
+			entrada = sc.nextInt();
+			if (entrada!=-99) valores.add(entrada);
+		}
+		return valores;
+	}
+
+	public static int calcularSuma(ArrayList<Integer> numeros){
+		int suma = 0;
+		Iterator<Integer> iteratorNumeros = numeros.iterator();
+		while (iteratorNumeros.hasNext()) {
+			suma += iteratorNumeros.next();
+		}
+
+		return suma;
+	}
+
+	public static int calcularPromedio(ArrayList<Integer> numeros){
+		int promedio;
+		int sumaDeNumeros = calcularSuma(numeros);
+		promedio = sumaDeNumeros/numeros.size();
+		return promedio;
+	}
+
+	public static void mostrarResultados(ArrayList<Integer> numeros,int suma, int media){
+		int numerosMayoresMedia = 0;
+    for (Integer numero : numeros) {
+      if (numero > media) {
+        numerosMayoresMedia++;
+      }
+    }
+		System.out.printf("Datos: %s \nSuma: %d \nMedia: %d \nCantidad mayores a media: %d", numeros, suma, media, numerosMayoresMedia);
+	}
+}
