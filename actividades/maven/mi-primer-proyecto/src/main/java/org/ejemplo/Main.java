4a8ed1e10ccdf7fa2790497289196a72b0c78025
Melissa Diaz <meli.diaz23@gmail.com>
2024-12-15 09:58:08 -0500

    Primer proyecto con Maven

---
+++ actividades/maven/mi-primer-proyecto/src/main/java/org/ejemplo/Main.java
@@ -0,0 +1,11 @@
+package org.ejemplo;
+
+public class Main {
+  public static void main(String[] args) {
+    saludo("Melissa", 33);
+  }
+
+  public static void saludo(String nombre, int edad){
+    System.out.println("Hola mi nombre es " + nombre + " y tengo " + edad + " años.");
+  }
+}
