6d6358a6c1af93059cc1a5610442584014fc3560
Melissa Diaz <meli.diaz23@gmail.com>
2025-01-06 17:23:31 -0500

    Script Actividad: Aplicando DML

---
+++ actividades/mysql/3_agregar_datos_empleados.sql
@@ -0,0 +1,42 @@
+USE mi_bd;
+
+INSERT INTO departamentos (nombre)
+VALUES ('Ventas'); 
+
+INSERT INTO departamentos (nombre)
+VALUES ('Recursos Humanos'); 
+
+-- Insertar empleado: Ana Rodriguez
+INSERT INTO empleados (nombre, apellido, edad, salario, correo_electronico, departamento_id)
+VALUES ('Ana', 'Rodriguez', 28, 3000.00, 'anarodriguez@mail.com', 1);
+
+-- Insertar empleado: Carlos López
+INSERT INTO empleados (nombre, apellido, edad, salario, correo_electronico, departamento_id)
+VALUES ('Carlos', 'López', 32, 3200.50, 'carloslopez@mail.com', 2);
+
+-- Insertar empleado: Laura Pérez
+INSERT INTO empleados (nombre, apellido, edad, salario, correo_electronico, departamento_id)
+VALUES ('Laura', 'Pérez', 26, 2800.75, 'lauraperez@mail.com', 1);
+
+-- Insertar empleado: Martín González
+INSERT INTO empleados (nombre, apellido, edad, salario, correo_electronico, departamento_id)
+VALUES ('Martín', 'González', 30, 3100.25, 'martingonzalez@mail.com', 2);
+
+-- Actualiza el salario del empleado con nombre "Ana" para aumentarlo en un 10%.
+UPDATE empleados
+SET salario = salario + (salario*0.10)
+WHERE nombre = 'Ana';
+
+-- Crea un departamento llamado “Contabilidad”. 
+INSERT INTO departamentos (nombre)
+VALUES ('Contabilidad'); 
+
+-- Cambia el departamento del empleado con nombre "Carlos" de "Recursos Humanos" a "Contabilidad"
+UPDATE empleados
+SET departamento_id = 3
+WHERE nombre = 'Carlos';
+
+-- Elimina al empleado con nombre "Laura"
+DELETE FROM empleados
+WHERE nombre = 'Laura';
+
