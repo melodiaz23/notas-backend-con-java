1520eb5f4e595bf5bcc273c548089998a6fd280f
Melissa Diaz <120139230+melodiaz23@users.noreply.github.com>
2025-01-30 16:45:39 -0500

    Creación proyecto, recuperar info bd

---
+++ actividades/06_JDBC/02_recuperar_info_bd/src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+import java.sql.*;
+
+import static org.example.config.DatabaseConfig.getConnection;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+
+    try (Connection conexion = getConnection()) {
+
+
+
+    } catch (Exception e) {
+
+    }
+  }
+}
+
+
+
