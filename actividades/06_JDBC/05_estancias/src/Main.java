19953bdd21e275300700255a54ed02ac95af6ec8
Melissa Diaz <120139230+melodiaz23@users.noreply.github.com>
2025-02-07 19:12:17 -0500

    Update Main.java

--- actividades/06_JDBC/05_estancias/src/Main.java
+++ actividades/06_JDBC/05_estancias/src/Main.java
@@ -4,11 +4,10 @@ public class Main {
   public static void main(String[] args) {
     ClienteDAO clientes = new ClienteDAO();
 
+
     try {
-
       clientes.listarClientes().forEach(System.out::println); // Metodo de referencia Clase::metodoDeInstancia
-
-
+      System.out.println(clientes.buscarClientePorCodigo(4));
     } catch (ClassNotFoundException e) {
       throw new RuntimeException(e);
     }
