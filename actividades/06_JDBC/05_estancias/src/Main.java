1bc51486545579171d4cbdaa527ecd75e6b846df
Melissa Diaz <120139230+melodiaz23@users.noreply.github.com>
2025-02-08 21:24:47 -0500

    Creación y validación de estancias en EstanciaDAO y EstanciaServicio

--- actividades/06_JDBC/05_estancias/src/Main.java
+++ actividades/06_JDBC/05_estancias/src/Main.java
@@ -1,29 +1,40 @@
 import entidades.Cliente;
+import entidades.Estancia;
+import persistencia.FamiliaDAO;
 import servicios.ClienteServicio;
+import servicios.EstanciaServicio;
+
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws SQLException {
     ClienteServicio clienteServicio = new ClienteServicio();
+    EstanciaServicio estanciaServicio = new EstanciaServicio();
 
-    try (Scanner scanner = new Scanner(System.in)) {
-    int opcion;
-    do {
-        menu();
-        opcion = scanner.nextInt();
-    } while (opcion != 2);
-
-    } catch (Exception e) {
-      throw new RuntimeException("Error: " +  e);
-    }
+//    try (Scanner scanner = new Scanner(System.in)) {
+//    int opcion;
+//    do {
+//        menu();
+//        opcion = scanner.nextInt();
+//    } while (opcion != 2);
+//    } catch (Exception e) {
+//      throw new RuntimeException("Error: " +  e);
+//    }
 
     try {
-      clienteServicio.listarTodosLosClientes().forEach(System.out::println); // Metodo de referencia Clase::metodoDeInstancia
-      Cliente clienteEncontrado = clienteServicio.buscarClientePorCodigo(4);
-      clienteServicio.guardarCliente(null);
-      System.out.println(clienteEncontrado);
-    } catch (ClassNotFoundException | SQLException e) {
+//      clienteServicio.listarTodosLosClientes().forEach(System.out::println); // Metodo de referencia Clase::metodoDeInstancia
+//      Cliente clienteEncontrado = clienteServicio.buscarClientePorCodigo(4);
+//      System.out.println(clienteEncontrado);
+//      clienteServicio.guardarCliente(null);
+      List<Estancia> estancias = estanciaServicio.listarTodasLasEstancias();
+      System.out.println(estancias);
+      System.out.println(estanciaServicio.buscarEstanciaPorId(3));
+
+    } catch (SQLException e) {
       throw new RuntimeException(e);
     }
   }
--- actividades/06_JDBC/05_estancias/src/servicios/EstanciaServicio.java
+++ actividades/06_JDBC/05_estancias/src/servicios/EstanciaServicio.java
@@ -1,4 +1,39 @@
 package servicios;
 
+import entidades.Estancia;
+import persistencia.EstanciaDAO;
+
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.List;
+
 public class EstanciaServicio {
+  private final EstanciaDAO estanciaDAO;
+
+  public EstanciaServicio() {
+    this.estanciaDAO = new EstanciaDAO();
+  }
+
+  public void crearNuevaEstancia(Estancia estancia) throws ParseException {
+    validarDatos(estancia);
+    estanciaDAO.crearEstancia(estancia);
+  }
+
+  private void validarDatos(Estancia estancia) {
+    if (estancia == null) throw new IllegalArgumentException("Estancia no puede ser nulo");
+    if (estancia.getIdCLiente() == 0) throw new IllegalArgumentException("Id del cliente debe ser un ID válido");
+    if (estancia.getIdCasa() == 0) throw new IllegalArgumentException("Id de la casa debe ser un ID válido");
+    if (estancia.getNombreHuesped() == null || estancia.getNombreHuesped().isEmpty()) throw new IllegalArgumentException("Nombre del huésped no puede ser nulo o vacío");
+    if (estancia.getFechaDesde() == null) throw new IllegalArgumentException("Fecha de inicio no puede ser nula");
+    if (estancia.getFechaHasta() == null) throw new IllegalArgumentException("Fecha de fin no puede ser nula");
+  }
+
+  public List<Estancia> listarTodasLasEstancias() throws SQLException {
+    return estanciaDAO.listarEstancias();
+  }
+
+  public Estancia buscarEstanciaPorId(int id) throws SQLException {
+    return estanciaDAO.buscarEstanciaPorId(id);
+  }
+
 }
