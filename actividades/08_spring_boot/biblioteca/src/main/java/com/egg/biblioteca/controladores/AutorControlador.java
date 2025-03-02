5829f849b1a3cd9e2280fd4154079d0715dd24b6
Melissa Diaz <120139230+melodiaz23@users.noreply.github.com>
2025-03-01 22:23:19 -0500

    Añadir mensajes de éxito y error en el formulario de registro de autores

--- actividades/08_spring_boot/biblioteca/src/main/java/com/egg/biblioteca/controladores/AutorControlador.java
+++ actividades/08_spring_boot/biblioteca/src/main/java/com/egg/biblioteca/controladores/AutorControlador.java
@@ -3,6 +3,7 @@ package com.egg.biblioteca.controladores;
 import com.egg.biblioteca.servicios.AutorServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,11 +25,13 @@ public class AutorControlador {
   }
 
   @PostMapping("/registro") // Same as th:action
-  public String registro(@RequestParam String nombre) {
+  public String registro(@RequestParam String nombre, ModelMap modelo) {
 
     try {
       autorServicio.crearAutor(nombre); // Persiste el autor
+      modelo.put("exito", "El autor fue cargado correctamente.");
     } catch (Exception e) {
+      modelo.put("error", e.getMessage());
       Logger.getLogger(AutorControlador.class.getName()).log(Level.SEVERE, null, e);
       return "autor_form.html";
     }
