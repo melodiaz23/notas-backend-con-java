a07042870d5ad286d4ada35bddc4762f48cb68fd
Melissa Diaz <120139230+melodiaz23@users.noreply.github.com>
2025-03-01 19:39:49 -0500

    Implementación del formulario de registro de editoriales y controlador correspondiente

---
+++ actividades/08_spring_boot/biblioteca/src/main/java/com/egg/biblioteca/controladores/EditorialControlador.java
@@ -0,0 +1,36 @@
+package com.egg.biblioteca.controladores;
+
+import com.egg.biblioteca.excepciones.MiException;
+import com.egg.biblioteca.servicios.EditorialServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Controller
+@RequestMapping("/editorial")
+public class EditorialControlador {
+  @Autowired
+  EditorialServicio editorialServicio;
+
+  @GetMapping("/registrar")
+  public String registrar(){
+    return "editorial_form.html";
+  }
+
+  @PostMapping("registro")
+  public String registro(@RequestParam String nombre){
+    try {
+      editorialServicio.crearEditorial(nombre);
+    } catch (MiException e) {
+      Logger.getLogger(AutorControlador.class.getName()).log(Level.SEVERE, null, e);
+      return "editorial_form.html";
+    }
+    return "index.html";
+  }
+}
